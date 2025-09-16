71c4994628719c677d69ed83918bcccb75031195
Notlighter <vanguardvalorent@gmail.com>
2025-09-16 12:10:11 +0500

    Commited

---
+++ Visual_Studio/Lab_4.dart
@@ -0,0 +1,36 @@
+import 'dart:math';
+
+// Base class
+class Shape {
+  double area() => 0;
+  double circumference() => 0;
+
+  Shape() {
+    print("Shape constructor called");
+  }
+}
+
+// Circle class extending Shape
+class Circle extends Shape {
+  double radius;
+
+  // Constructor using sugar syntax with call to super
+  Circle(this.radius) : super();
+
+  // Override area
+  @override
+  double area() => pi * radius * radius;
+
+  // Override circumference
+  @override
+  double circumference() => 2 * pi * radius;
+}
+
+void main() {
+  // Create Circle object
+  Circle c = Circle(5.0);
+
+  print("Circle with radius: ${c.radius}");
+  print("Area: ${c.area()}");
+  print("Circumference: ${c.circumference()}");
+}
